ec0462d6c08d885d89378ad9c5986e6633ba86b9
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2024-01-23 21:14:45 -0500

    svg work

--- SVGReplication/src/main.js
+++ SVGReplication/src/main.js
@@ -1,10 +1,43 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const main = document.querySelector("#main");
 
-ctx.fillStyle = "green";
+const createSVG = (width, height) => {
 
-for(let i = 0; i < 10000; i++) {
-    
-    ctx.fillStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
-    ctx.fillRect(Math.random() * 300, Math.random() * 150, 2, 2);
+  return `
+    <svg width="${width}" height="${height}">
+        <rect width="${width}" height="${height}" fill="none" stroke="black" />
+        ${drawSquare(10, 10, 20, 20, "red", 5)}
+        ${drawSquare(5, 5, 20, 20, "red", 5)}
+    </svg>
+  `;
 }
+
+const drawSquare = (x, y, width, height, color, angle=0) => {
+
+    //I have to add it all to a concatinated string
+    let sqrString = ``;
+    
+    for(let i = width; i > 0; i--) {
+        console.log(i);
+        sqrString += drawRect(x+i, y+i, i, i, color, angle);
+    }
+    console.log(sqrString);
+
+  return sqrString;
+}
+
+const drawRect = (x, y, width, height, color) => {
+    return `
+    <path 
+        fill="none"
+        stroke="${color}"
+        d="
+        M${x}, ${y}
+        L${x + width}, ${y}
+        L${x + width}, ${y + height}
+        L${x}, ${y + height}
+        z"
+    </path>`;
+}
+// 
+
+main.outerHTML = createSVG(100, 100);
