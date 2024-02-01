51a8d103f3ea5fb2530117ab36b76b48f6abfcc9
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2024-02-01 11:35:49 -0500

    Update main.js

--- toolbox_3_transforms/src/main.js
+++ toolbox_3_transforms/src/main.js
@@ -1,34 +1,241 @@
 const main = document.querySelector("#main");
 
+const canvasWidth = 500;
+const canvasHeight = 500;
+
 const createSVG = (width, height) => {
+    const skewed = new Geometry(0,0,50,50,"black",0,1);
+    console.log(skewed);
+    let prect = new polyRect(0,0,50,50);
 
-    let myRot = rotate(30,"square",{x:50,y:50})
-
-    console.log(myRot);
-
+    let grid = new drawGrid(0,0,10,10,50,50,10,10);
   return `
     <svg width="${width}" height="${height}">
-        <rect width="${width}" height="${height}" fill="none" stroke="black" />
-        ${
-            drawFillSquare(12, 45, 20, 20, "firebrick", 5) +
-            drawFillSquare(22, 18, 20, 20, "#551625", 60) +
-            drawFillSquare(36, 17, 20, 20, "#BB2A2D", 88, .98) +
-            drawFillSquare(20, 31, 20, 20, "#E84139", 45) +
-            drawFillSquare(58, 52, 20, 20, "#551625", 5) +
-            drawFillSquare(49, 57, 20, 20, "#551625", 63) +
-            drawFillSquare(48, 35, 20, 20, "#E84139", 78) +
-            drawFillSquare(60, 73, 20, 20, "#BB2A2D", 45) +
-            drawFillSquare(73, 74, 20, 20, "#d70000", 12) 
-            
-        }
+        <rect width="${width}"
+         height="${height}" 
+         fill="none" 
+         stroke="black" 
+        />
         
+        ${prect.draw()}
+        ${grid.draw()}
 
     </svg>
   `;
 }
 
-const drawSquareGrid = (rows, columns, square, transforms, randMag, spacing) => {
+let objArr = [];
 
+function r() {
+    return Math.random()*4-2;
+}
+
+class DesShape {
+    constructor(geo, transforms, appearence) {
+        this.geo = geo;
+        this.transforms = transforms;
+        this.appearence = appearence;
+    }
+
+    draw = () => {
+    }
+}
+
+let vector = {
+    x: 0,
+    y: 0
+}
+
+let rotate = {
+    point: vector,
+    angle: 0,
+}
+
+let scale = {
+    mag: 1,
+}
+
+class Transforms {
+    constructor(translate, rotate, scale) {
+        this.tr = translate;
+        this.rot = rotate;
+        this.sc = scale;
+    }
+
+    get translate() {
+        return this.tr;
+    }
+
+    set translate(value) {
+        this.tr = value;
+    }
+
+    get rotate() {
+        return this.rot;
+    }
+
+    set rotate(value) {
+        this.rot = value;
+    }
+
+    get scale() {
+        return this.sc;
+    }
+
+    set scale(value) {
+        this.sc = value;
+    }
+}
+
+class Appearence {
+    constructor(color, opacity, stroke, strokeWidth) {
+        this.col = color;
+        this.op = opacity;
+        this.str = stroke;
+        this.strWidth = strokeWidth;
+    }
+
+    get color() {
+        return this.col;
+    }
+
+    set color(color) {
+        this.col = color;
+    }
+
+    get opacity() {
+        return this.op;
+    }
+
+    set opacity(opacity) {
+        this.op = opacity;
+    }
+
+    get stroke() {
+        return this.str;
+    }
+
+    set stroke(stroke) {
+        this.str = stroke;
+    }
+
+    get strokeWidth() {
+        return this.strWidth;
+    }
+
+    set strokeWidth(strokeWidth) {
+        this.strWidth = strokeWidth;
+    }
+}
+
+class Geometry {
+    constructor(pos, size) {
+        this.pos = pos;
+        this.s = size;
+    }
+
+    get position() {
+        return this.pos;
+    }
+
+    set position(value) {
+        this.pos = value;
+    }
+
+    get size() {
+        return this.s;
+    }
+
+    set size(value) {
+        this.s = value;
+    }
+}
+
+
+class drawGrid {
+    constructor(startX, startY, cols, rows, colWidth=0, rowHeight=0) {
+        this.startX = startX;
+        this.startY = startY;
+        this.cols = cols;
+        this.rows = rows;
+        this.colWidth = colWidth;
+        this.rowHeight = rowHeight;
+        this.drawStr = ``;
+        
+    }
+
+    
+
+    draw = () => {
+        for(let i = 0; i < this.cols; i++) {
+            for(let j = 0; j < this.rows; j++){
+                //let obj = new polySpiral(this.startX + (i*this.colWidth), this.startY + (j*this.rowHeight), this.colWidth)//
+                let obj = new polyRect(this.startX + (i*this.colWidth), this.startY + (j*this.rowHeight), this.colWidth, this.rowHeight);
+                this.drawStr += obj.draw();
+                this.drawStr += polySpiral(this.startX + (i*this.colWidth), this.startY + (j*this.rowHeight), this.colWidth);
+                objArr.push(obj);
+            }
+        }
+
+        return `<g>${this.drawStr}</g>`;
+    }
+}
+
+const polySpiral = (x,y,size) => {
+    let psStr = ``;
+    let s = size;
+
+    let randInt = Math.floor(Math.random()*10);
+    for(let i = 0; i < randInt + randInt; i++){
+        let spir = new polyRect(x+(i*2.5), y+(i*2.5), s, s);
+        psStr += spir.draw();
+        s -= (size)/(10);
+    }
+
+    return `<g>${psStr}</g>`;
+}
+
+
+class polyRect {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw = () => {
+        console.log("drawing");
+        return `
+        <polyline
+            stroke="black"
+            fill="none"
+            points="
+            ${this.x}, ${this.y}
+            ${this.x + r() + this.width}, ${this.y + r()}
+            ${this.x + r() + this.width}, ${this.y + r() + this.height}
+            ${this.x}, ${this.y + r() + this.height}
+            ${this.x}, ${this.y}"
+        />`
+    };
+}
+
+
+const drawRow = (startX, startY, squareFun, spacing, color, angle=0, opacity=1) => {
+    let sqrString = ``;
+    let unitX = startX;
+    
+    for(let i = 0; i < 10 ; i++) {
+        sqrString += squareFun(unitX, startY, 50, 50, color, angle, opacity);
+        unitX += spacing;
+    }
+  return `<g>${sqrString}</g>`;
+}
+
+const drawSquareGrid = (columns, square, transforms, randMag, spacing) => {
+    for(let i = 0; i < columns; i++) {
+        drawSquareRow(i*spacing, 0, square, square, "black", 0, 1);
+    }
 }
 
 const drawFillSquare = (x, y, width, height, color, angle=0, opacity=1) => {
@@ -40,19 +247,8 @@ const drawFillSquare = (x, y, width, height, color, angle=0, opacity=1) => {
     }
 
   return `<g transform="rotate(${angle},${x},${y})", opacity="${opacity}">${sqrString}</g>`;
-}
+};
 
-const rotate = (degrees, obj, point={x:0,y:0}) => ({
-   type: 'rotation',
-   degrees,
-   point,
-   object: obj 
-});
-
-const translate = (offsetX, offsetY) => ({
-    offsetX,
-    offsetY
-});
 
 const drawRect = (x, y, width, height, color) => {
     return `
@@ -66,7 +262,10 @@ const drawRect = (x, y, width, height, color) => {
         L${x}, ${y + height}
         z"
     />`;
-}
+};
+
+
+
 
 const drawCircle = (x,y,r) => {
     return `
@@ -92,7 +291,7 @@ const drawEllipse = (x,y,r,h) => {
         C ${x-r}, ${y-(r+h)}, ${x+r}, ${y-(r+h)}, ${x+r}, ${y}"
         stroke="black"
     />`;
-}
+};
 
 const drawLine = (x1,y1,x2,y2) => {
     return `
@@ -119,4 +318,4 @@ const drawPolygon = (x,y,x1,y1,x2,y2) => {
 }
 
 
-main.outerHTML = createSVG(500, 500);
+main.outerHTML = createSVG(canvasWidth, canvasHeight);
